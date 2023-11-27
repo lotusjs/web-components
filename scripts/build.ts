ddf4280e5e6670f348cfd9702ca98d1d31596708
wangxingkang <wang_xingkang@qq.com>
2023-11-27 20:58:38 +0800

    chore: 脚本部分提交

--- scripts/build.ts
+++ scripts/build.ts
@@ -10,27 +10,32 @@ import fs from 'fs/promises';
 import { readFileSync } from 'fs';
 import * as path from 'path';
 import getPort, { portNumbers } from 'get-port';
-import ora from 'ora';
 import util from 'util';
 import { replace } from 'esbuild-plugin-replace';
+import { nextTask } from './utils';
+
+import type { ChildProcessWithoutNullStreams } from 'child_process';
+
+interface ChildResult {
+  child: ChildProcessWithoutNullStreams;
+  output: string[];
+}
 
 const { serve } = commandLineArgs([{ name: 'serve', type: Boolean }]);
 const outdir = 'dist';
 const cdndir = 'cdn';
 const sitedir = '_site';
-const spinner = ora({ hideCursor: false }).start();
 const execPromise = util.promisify(exec);
 
-console.log(serve);
+const bundleDirectories = [cdndir, outdir];
 
 async function buildTheDocs(watch = false) {
-  return new Promise(async (resolve, reject) => {
+  return new Promise<ChildResult>(async (resolve, reject) => {
     const afterSignal = '[eleventy.after]';
+
     const errorSignal = 'Original error stack trace:';
-
     const args = ['@11ty/eleventy', '--quiet'];
-
-    const output = [];
+    const output: string[] = [];
 
     if (watch) {
       args.push('--watch');
@@ -43,9 +48,41 @@ async function buildTheDocs(watch = false) {
       shell: true
     });
 
+    child.stdout.on('data', (data) => {
+      if (data.includes(afterSignal)) return; // don't log the signal
+      output.push((data).toString());
+    });
+
+    child.stderr.on('data', data => {
+      output.push(data.toString());
+    });
+
+    if (watch) {
+      // The process doesn't terminate in watch mode so, before resolving, we listen for a known signal in stdout that
+      // tells us when the first build completes.
+      child.stdout.on('data', data => {
+        if (data.includes(afterSignal)) {
+          resolve({ child, output });
+        }
+      });
+
+      child.stderr.on('data', data => {
+        if (data.includes(errorSignal)) {
+          // This closes the dev server, not sure if thats what we want?
+          reject(output);
+        }
+      });
+    } else {
+      child.on('close', () => {
+        resolve({ child, output });
+      });
+    }
   })
 }
 
-if (serve) {
-
-}
+(async () => {
+  await nextTask('Cleaning up the previous build', async () => {
+    await Promise.all([deleteAsync(sitedir), ...bundleDirectories.map(dir => deleteAsync(dir))]);
+    await fs.mkdir(outdir, { recursive: true });
+  });
+})()
