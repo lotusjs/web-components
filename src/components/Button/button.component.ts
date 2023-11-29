5b75959da9be9c55601eac94abdabccd5edbee8c
wangxingkang <wang_xingkang@qq.com>
2023-11-29 14:12:06 +0800

    feat(button): 部分代码提交

--- src/components/Button/button.component.ts
+++ src/components/Button/button.component.ts
@@ -1,7 +1,8 @@
 import { html } from 'lit';
 import { property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
-import { ShoelaceElement } from '../../internal/ShoelaceElement';
+import { HasSlotController } from '../../internal/slot.js';
+import { ShoelaceElement } from '../../internal/ShoelaceElement.js';
 import styles from './button.styles';
 
 import type { CSSResultGroup } from 'lit';
@@ -9,15 +10,29 @@ import type { CSSResultGroup } from 'lit';
 export default class Button extends ShoelaceElement {
   static styles: CSSResultGroup = styles;
 
-  @property()
-  name?: string = 'World';
+  private readonly hasSlotController = new HasSlotController(this, '[default]', 'prefix', 'suffix');
+
+  /** The button's theme variant. */
+  @property({ reflect: true }) variant: 'default' | 'primary' | 'success' = 'default';
+
+  /** The button's size. */
+  @property({ reflect: true }) size: 'small' | 'medium' | 'large' = 'medium';
+
+  /** Draws an outlined button. */
+  @property({ type: Boolean, reflect: true }) outline = false;
 
   render() {
     return html`
       <button
-        class=${classMap({ button: true })}
+        class=${classMap({
+          button: true,
+          'button--default': this.variant === 'default',
+          'button--medium': this.size === 'medium',
+          'button--standard': !this.outline,
+          'button--has-label': this.hasSlotController.test('[default]'),
+        })}
       >
-        Hello, ${this.name}
+        <slot part="label" class="button__label"></slot>
       </button>
     `;
   }
--- src/components/Button/button.styles.ts
+++ src/components/Button/button.styles.ts
@@ -1,6 +1,9 @@
 import { css } from 'lit';
+import componentStyles from '../../styles/component.styles.js';
 
 export default css`
+  ${componentStyles}
+
   :host {
     display: inline-block;
     position: relative;
@@ -10,5 +13,40 @@ export default css`
 
   .button {
     display: inline-flex;
+    align-items: stretch;
+    justify-content: center;
+    width: 100%;
+    border-style: solid;
+    border-width: var(--l-input-border-width);
+    font-family: var(--l-input-font-family);
+    font-weight: var(--l-font-weight-semibold);
+    text-decoration: none;
+    user-select: none;
+    white-space: nowrap;
+    vertical-align: middle;
+    padding: 0;
+  }
+
+  /* Default */
+  .button--standard.button--default {
+    background-color: var(--l-color-neutral-0);
+    border-color: var(--l-color-neutral-300);
+    color: var(--l-color-neutral-700);
+  }
+
+  .button--medium {
+    height: auto;
+    min-height: var(--l-input-height-medium);
+    font-size: var(--l-button-font-size-medium);
+    line-height: calc(var(--l-input-height-medium) - var(--l-input-border-width) * 2);
+    border-radius: var(--l-input-border-radius-medium);
+  }
+
+  .button__label {
+    display: inline-block;
+  }
+
+  .button--has-label.button--medium .button__label {
+    padding: 0 var(--l-spacing-medium);
   }
 `
